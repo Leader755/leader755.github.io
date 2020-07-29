eaabdff622dd4787e0c7602f2cf90abc43db61ae
account name <account email>
2020-07-29 23:43:00 +0800

    Site updated: 2020-07-29 23:43:00

--- js/matery.js
+++ js/matery.js
@@ -80,8 +80,9 @@ $(function () {
 
       //为文章内的图片添加no-referrer来隐藏referer（解决第三方图片外链不显示问题）
       $(this).attr('referrerpolicy', 'no-referrer');
-      $('.img-item').attr('referrerpolicy', 'no-referrer');
-
+      // $('.img-item').attr('referrerpolicy', 'no-referrer');
+      //为文章内的图片添加no-referrer来隐藏referer（解决第三方图片外链不显示问题）
+      $('.lg-thumb-item').find('img').attr('referrerpolicy', 'no-referrer');
 
       // 图片添加字幕
       let alt = $(this).attr('alt');
@@ -175,4 +176,6 @@ $(function () {
       $('.m-nav-item.m-nav-show').removeClass('m-nav-show');
     }
   });
+
+
 });
