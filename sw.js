0510b17239fc413c5880a60d5b45452dff106d66
account name <account email>
2020-08-02 13:13:14 +0800

    Site updated: 2020-08-02 13:13:13

--- sw.js
+++ sw.js
@@ -32,13 +32,13 @@ var precacheUrls = [];
 
   precacheUrls.push('/leader755/896b4ef0.html');
 
-  precacheUrls.push('/leader755/dfc8c080.html');
-
   precacheUrls.push('/leader755/38b922c9.html');
 
-  precacheUrls.push('/leader755/bad88671.html');
+  precacheUrls.push('/leader755/dfc8c080.html');
 
   precacheUrls.push('/leader755/b0c81b21.html');
+
+  precacheUrls.push('/leader755/bad88671.html');
 
   precacheUrls.push('/leader755/5c84a3bd.html');
 
