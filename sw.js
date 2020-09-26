12bd22efc2069660c55bb09e95a7874b5d0090d0
account name <account email>
2020-09-26 23:03:17 +0800

    Site updated: 2020-09-26 23:03:16

--- sw.js
+++ sw.js
@@ -26,13 +26,13 @@ var precacheUrls = [];
 
   precacheUrls.push('/leader755/7894.html');
 
-  precacheUrls.push('/leader755/10760.html');
-
   precacheUrls.push('/leader755/38612.html');
 
-  precacheUrls.push('/leader755/40372.html');
+  precacheUrls.push('/leader755/10760.html');
 
   precacheUrls.push('/leader755/58848.html');
+
+  precacheUrls.push('/leader755/40372.html');
 
   precacheUrls.push('/leader755/52316.html');
 
@@ -42,9 +42,9 @@ var precacheUrls = [];
 
   precacheUrls.push('/leader755/30871.html');
 
-  precacheUrls.push('/leader755/50207.html');
-
   precacheUrls.push('/leader755/14064.html');
+
+  precacheUrls.push('/leader755/50207.html');
 
   precacheUrls.push('/leader755/52181.html');
 
